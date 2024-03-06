2c75ec4dc3878cf06b12de39f072b75e5b7553c1
iren42 <isabelleren618@gmail.com>
2024-03-06 14:07:49 +0100

    up: pong physics is more complex. TODO: tests and clean code

--- srcs/requirements/front/pong.js
+++ srcs/requirements/front/pong.js
@@ -16,25 +16,39 @@ const pong = {
 
 	ball : {
 		width : 10,
-		height : 10,
+		height : 10, // must be equal to width
 		color : "#0000FF",
 		posX : 700 / 2,
 		posY : 400 / 2,
-		speed : 1,
-		velocityX : 3,
-		velocityY : 3,
+		speed : 5,
+		offset : 0,
+		angle : 0,
+		velocityX : 0,
+		velocityY : 0,
 		bounceOffWall : function()
 		{
-			if ( this.posY + this.height > pong.groundHeight || this.posY < 0  )
+			if (this.posY + this.height > pong.groundHeight || this.posY < 0)
 				this.velocityY *= -1;  
-			if ( this.posX + this.width > pong.groundWidth || this.posX < 0  )
+			if (this.posX + this.width > pong.groundWidth || this.posX < 0)
 				this.velocityX *= -1;  
 		},
 		move : function()
 		{
-			if (pong.paddleL.detectCollision(pong.ball)
-				|| pong.paddleR.detectCollision(pong.ball))
+			if (pong.paddleL.detectCollision(pong.ball))
 			{
+				// calcul de la trajectoire de la balle
+				this.offset = (this.posY + this.width - pong.paddleL.posY) / (pong.paddleL.height + this.width);
+				this.angle = 0.25 * Math.PI * (2 * this.offset - 1);
+
+				this.velocityY = this.speed * Math.sin(this.angle);
+				this.velocityX *= -1;
+			}
+			if (pong.paddleR.detectCollision(pong.ball))
+			{
+				this.offset = (this.posY + this.width - pong.paddleR.posY) / (pong.paddleR.height + this.width);
+				this.angle = 0.25 * Math.PI * (2 * this.offset - 1);
+
+				this.velocityY = this.speed * Math.sin(this.angle);
 				this.velocityX *= -1;
 			}
 			// ball must never go through our paddles
@@ -43,24 +57,69 @@ const pong = {
 				|| this.posX + this.width > pong.paddleR.posX - 10
 				&& this.velocityX > 0)
 			{
+				console.log(this.velocityX + ","  + this.velocityY + " velx vely");
 				let i = 0;
-				/* console.log("small increment"); */
-				while (i++ < Math.abs(this.velocityX)
-					&& !(pong.paddleL.detectCollision(pong.ball)
-						|| pong.paddleR.detectCollision(pong.ball)))
+				let higherVel = Math.abs(this.velocityY);
+				let diff = (Math.abs(this.velocityX) - Math.abs(this.velocityY));
+				if (diff > 0)
+					higherVel = Math.abs(this.velocityX);
+				console.log("diff = " + diff);
+				let buf = (diff);
+				while (i++ < higherVel
+					&& (!(pong.paddleL.detectCollision(pong.ball)
+						|| pong.paddleR.detectCollision(pong.ball))))
 				{
-					if (this.velocityX > 0)
-						this.posX++;
+					if (buf == 0) // posX and posY increment at the same speed 
+					{
+
+						if (this.velocityX > 0)
+							this.posX++;
+						else
+							this.posX--;
+						if (this.velocityY > 0)
+							this.posY++;
+						else
+							this.posY--;
+					}
+					else if (buf > 0) // posX increments faster than posY
+					{
+						if (this.velocityX > 0)
+						{
+							this.posX++;
+						}
+						else
+						{
+							this.posX--;
+						}
+						if (this.velocityY > 0 && buf == 1)
+							this.posY++;
+						else if (this.velocityY < 0 && buf == 1)
+							this.posY--;
+						else
+							;
+						buf--;
+					}
 					else
-						this.posX--;
-					if (this.velocityY > 0)
-						this.posY++;
-					else
-						this.posY--;
+					{
+						if (this.velocityX > 0 && buf == -1)
+						{
+							this.posX++;
+						}
+						else if (this.velocityX < 0 && buf == -1)
+							this.posX--;
+						else
+							;
+						if (this.velocityY > 0)
+							this.posY++;
+						else
+							this.posY--;
+						buf++;
+					}
 				}
 			}
 			else
 			{
+
 				this.posY += this.velocityY;
 				this.posX += this.velocityX;
 
@@ -152,7 +211,7 @@ const pong = {
 		pong.clearLayer(pong.ballPaddlesLayer);
 		pong.moveBall();
 		pong.movePaddles();
-		pong.updateScore();
+		pong.winLoseSystem();
 		pong.displayBall();
 		pong.displayPaddles();
 		if (pong.code["Space"].pressed)
@@ -181,6 +240,17 @@ const pong = {
 		window.onkeydown = onKeyDownFunction;
 		window.onkeyup = onKeyUpFunction;
 	},
+
+	detectCollision : function(obj1, obj2)
+	{
+		if (obj1.posX <= obj2.posX + obj2.width &&
+			obj1.posX + obj1.width >= obj2.posX &&
+			obj1.posY <= obj2.posY + obj2.height &&
+			obj1.height + obj1.posY >= obj2.posY)
+			return true;
+		return false;
+	},
+
 	displayScore : function(scorePlayer1, scorePlayer2)
 	{
 		pong.display.drawTextInLayer(this.scoreLayer, this.scorePlayer1, "30px Arial", "#FF0000", 40, 30);
@@ -203,20 +273,24 @@ const pong = {
 	},
 	randomizeBallDirection()
 	{
+		this.ball.velocityX = this.ball.speed;
+		this.ball.velocityY = 0;
 		if ((Math.floor(Math.random() * 2)) % 2)
 			this.ball.velocityX *= -1;
+		console.log("vel x = " + this.ball.velocityX + "vel y = " + this.ball.velocityY );
 	},
 	centerBall : function()
 	{
 		this.ball.posX = this.groundWidth / 2;
 		this.ball.posY = this.groundHeight / 2;
 	},
-	updateScore : function()
+	winLoseSystem : function()
 	{
 		// left player gagne 1 point
 		if (pong.ball.posX + pong.ball.width >= this.groundWidth)
 		{
 			this.scorePlayer1++;
+			this.randomizeBallDirection();
 			// ball se dirige vers le gagnant
 			if (this.ball.velocityX > 0)
 				this.ball.velocityX *= -1;
@@ -228,6 +302,7 @@ const pong = {
 		else if (pong.ball.posX <= 0)
 		{
 			this.scorePlayer2++;
+			this.randomizeBallDirection();
 			if (this.ball.velocityX < 0)
 				this.ball.velocityX *= -1;
 			this.centerBall();
